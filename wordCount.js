df6f52a70f456d12d2048ab64d5d3343483bf8dd
Anthony Zotti <theillbehaviored@gmail.com>
2014-06-12 20:00:56 -0400

    Difficulty: Easy

---
+++ wordCount.js
@@ -0,0 +1,8 @@
+function WordCount(str) {  
+  return str.match(/[a-zA-Z0-9]+/g).length;
+
+}
+
+// keep this function call here 
+// // to see how to enter arguments in JavaScript scroll down
+WordCount(readline());  
