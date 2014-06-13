b44c440f0f1f659447b51623c79de65440b88149
Anthony Zotti <theillbehaviored@gmail.com>
2014-06-13 19:48:18 -0400

    Add utility functions for high level programming
    -Change processParagraph incrementor to use header++ instead of
    assignment incrementing to be more JS like
    
    REDUCE FUNCTION
    
    -Receives a basic addition function, an array of numbers, and a base of
    
    -The base is there so the code has something to add to. In other words
    base is total
    
    -function(element) has two parts. Function is representational of the
    action that is being executed and element is each element within the
    array of numbers passed
    
    -combine(base,element) can  be thought of as:  base = base +
    element". Combine is nothing but plus operator in essence
    
    -sum function is highly redundant. It does nothing but organize the
    variables and call the reduce function

--- processParagraph.js
+++ processParagraph.js
@@ -1,8 +1,43 @@
+
+
+
 function processParagraphs(p){
   var header = 0;
   while (p[0] == "%"){
-    header += 1;
+    header++;
     p = p.slice(1);
   }
   return {type: (header == 0? "p" : "h" + header), content: p};
 }
+
+
+
+//Utility functions
+
+function forEach(array, action){
+  for (var i = 0;i < array.length;i++){
+    action(array[i]);
+  }
+}
+
+function reduce(combine, base, number_array){
+  forEach(number_array, function(element){
+    base = combine(base,element);
+  });
+  return base;
+}
+
+function add(a,b){
+  return a + b;
+}
+
+function sum(number_array){
+  return reduce(add,0,number_array);
+}
+//Add is for 2 numbers
+//Sum is for multiple numbers
+//reduce = sumLoop
+//combine = add
+//base = 0
+//number_array = userInput
+//element = each element within the number array
